128781cb95e598f9ba1f21cd438a8da0377a37bd
Sylvanus-the-sorcerer <103826382+sepehrsylvanus@users.noreply.github.com>
2024-10-29 17:39:24 +0300

    navbar

--- components/index.js
+++ components/index.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line import/no-cycle
 import Navbar from './Navbar';
 import Footer from './Footer';
+import Button from './Button';
 
-export { Navbar, Footer };
+export { Navbar, Footer, Button };
